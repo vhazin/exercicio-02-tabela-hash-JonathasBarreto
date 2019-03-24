b544a9129b397badae65e40591b523641e366e15
JonathasBarreto <42970380+JonathasBarreto@users.noreply.github.com>
2019-03-24 16:18:18 -0300

    Update jlsb.c

--- jlsb.c
+++ jlsb.c
@@ -1,82 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+typedef int hash;
 
-int main()
-{
-    int q_teste, q_end, q_chaves, chave, end, end_ant;
-    int g = 0;
+hash main(void){
+    hash q_testes, q_end, q_chaves, chave,  end, g;
 
-    scanf("%d", &q_teste);// numero de testes q serão feitos
+    scanf("%d", &q_testes);
 
+    for(hash i=0; i <= q_testes - 1; i++){
+        scanf("%d %d", &q_end, &q_chaves);
 
-    for(int i = 0; i<q_teste; i++){//for para armazenar os testes
-
-        scanf("%d %d", &q_end, &q_chaves);// recebe quantidade de endereços e chaves
-
-        int v[q_end][q_chaves];// cria a matriz com o tramanho do endereço e das chaves
-
-        for(int a = 0; a<q_end; a++){// zera toda a matriz
-
-            for(int b = 0; b<q_chaves; b++){
-
-                v[a][b] = 0;
-            }
-        }
+        hash chaves_v[q_chaves], v[q_end][q_chaves];
         
-        for(int f = 0; f<q_chaves; f++){/* recebe as chaves de acordo com 
-                o tamanho de chaves pedido*/
-
+        for (hash a = 0; a <= q_chaves - 1; a++){
             scanf("%d", &chave);
+            chaves_v[a] = chave;
+        }
 
-            end = chave % q_end;// hash função, vai dar um endereço
+        for(hash n = 0; n <= q_end - 1; n++){
+            g = 0;
             
-            if(end_ant != end){/* zera G toda vez que o 
-                endereço for diferente que o endereço anterior*/
+            for(int p = 0; p <= q_chaves - 1; p++){
+                end = chaves_v[p] % q_end;
                 
-                g = 0;
+                if(end != n) v[n][g] = 0;
+                    else v[n][g] = chaves_v[p];
+                    
+                g++;
+
             }
             
-            end_ant = end;// armazena no endereo como anterior
+            if(n == 0) printf("%d -> ", n);
+                else  printf("\n%d -> ", n);
 
-            while(v[end][g]!=0){// tratamento de colisão
-                g++;
-            }
-            v[end][g]= chave;
-        }
-        for(int n = 0; n<q_end; n++){// condições do print, for da linha
-            if(n != 0){// pula linha sempre q for diferente do inicio
+            for(hash d = 0; d <= q_chaves - 1; d++){
+                if(v[n][d]) printf("%d -> ", v[n][d]);
 
-                printf("\n");
-
-            }
-
-            printf("%d", n);
-
-            for(int m = 0; m<q_chaves; m++){// for da coluna
-
-                if (v[n][m] != 0){ /* printa isso se existir um 
-                        valor na posião da matriz*/
-
-                    printf(" -> %d", v[n][m]);
-                }
-
-                else{
-
-                    printf(" -> \\");// printa / se for vazio
-                    if(n == q_end - 1 && i < q_teste-1){/*pula linha uma vez ao
-                            final da matriz antes do ultimo teste*/
-
-                        printf("\n");
-                    }
-                    break;
-                }
-
-            }
-        }
-        if(i < q_teste-1){/*pula linha uma vez antes do ultimo teste*/
-
-            printf("\n");
-        }
-    }//fecha o for dos testes
-    return 0;
+            }printf("\\");
+            
+            if (n==q_end-1 && i!=q_testes-1) printf("\n");
+        }printf("\n");
+    }
 }
